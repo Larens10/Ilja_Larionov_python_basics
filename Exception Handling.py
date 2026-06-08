be555beeaa6ba237dfd9be17db63d1c49585de5d
Ilya_Larionov <Larens999@mail.ru>
2026-06-08 22:31:54 +0500

    Initial commit

---
+++ Exception Handling.py
@@ -0,0 +1,16 @@
+# ДЗ: Обработка исключений
+
+try:
+    number_one = float(input("Введите числитель (через точку): "))
+    number_two = float(input("Введите знаменатель (через точку): "))
+    result = number_one / number_two
+except ValueError:
+    print('\n', "Ошибка: Некорректный ввод числа")
+except ZeroDivisionError:
+    print('\n', "Ошибка: Попытка деления на ноль")
+# except:
+#     print("Непредвиденная ошибка")
+else:
+    print('\n', f"Выражение {number_one} / {number_two} = {result}")
+finally:
+    print('\n', "Расчёт завершён. Программа закрыта")
