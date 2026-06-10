bb904eed8ad9d9b26fc2e7d6fd06d1bbff3b0dda
Ilya_Larionov <Larens999@mail.ru>
2026-06-10 23:52:09 +0500

    ДЗ: Калькулятор

--- main.py
+++ main.py
@@ -1,2 +1,28 @@
 # ДЗ: Калькулятор
 
+result = None
+try:
+    number_one = float(input("Введите 1-е число: "))
+    number_two = float(input("Введите 2-е число: "))
+except ValueError:
+    print('\n', "Ошибка: Некорректный ввод числа")
+else:
+    enable_operation = '+-*/'
+    operation = input("Доступные действия:\n'+' - сложение\n'-' - вычитание\n"
+                      "'*' - умножение\n'/' - деление\nВведите математический символ: ")
+    if operation in enable_operation:
+        if operation == '+':
+            result = number_one + number_two
+        elif operation == '-':
+            result = number_one - number_two
+        elif operation == '*':
+            result = number_one * number_two
+        else:
+            try:
+                result = number_one / number_two
+            except ZeroDivisionError:
+                print('\n', "Ошибка: Попытка деления на ноль")
+        if result is not None:
+            print('\n', f"{number_one} {operation} {number_two} = {result}")
+    else:
+        print('\n', "Ошибка: Введён неверный математический символ")
