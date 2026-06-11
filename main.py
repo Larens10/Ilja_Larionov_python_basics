9c73cee4bae0c02f550e7ebdcaccc2999b009f9b
Ilya_Larionov <Larens999@mail.ru>
2026-06-12 00:22:18 +0500

    ДЗ: Калькулятор (v.2)

--- main.py
+++ main.py
@@ -1,28 +1,31 @@
 # ДЗ: Калькулятор
 
-result = None
+class OperationError(Exception):
+    pass
+
+
 try:
     number_one = float(input("Введите 1-е число: "))
     number_two = float(input("Введите 2-е число: "))
-except ValueError:
-    print('\n', "Ошибка: Некорректный ввод числа")
-else:
-    enable_operation = '+-*/'
     operation = input("Доступные действия:\n'+' - сложение\n'-' - вычитание\n"
                       "'*' - умножение\n'/' - деление\nВведите математический символ: ")
-    if operation in enable_operation:
-        if operation == '+':
-            result = number_one + number_two
-        elif operation == '-':
-            result = number_one - number_two
-        elif operation == '*':
-            result = number_one * number_two
-        else:
-            try:
-                result = number_one / number_two
-            except ZeroDivisionError:
-                print('\n', "Ошибка: Попытка деления на ноль")
-        if result is not None:
-            print('\n', f"{number_one} {operation} {number_two} = {result}")
+
+    if operation == '+':
+        result = number_one + number_two
+    elif operation == '-':
+        result = number_one - number_two
+    elif operation == '*':
+        result = number_one * number_two
+    elif operation == '/':
+        result = number_one / number_two
     else:
-        print('\n', "Ошибка: Введён неверный математический символ")
+        raise OperationError("\nОшибка: Введён неверный математический символ")
+
+except ValueError:
+    print("\nОшибка: Некорректный ввод числа")
+except ZeroDivisionError:
+    print("\nОшибка: Попытка деления на ноль")
+except OperationError as e:
+    print(e)
+else:
+    print(f"\n{number_one} {operation} {number_two} = {result}")
